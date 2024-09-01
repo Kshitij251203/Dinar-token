e836d450a0425a1ca26539a3319a80699898d4af
Kshitij251203 <133272823+Kshitij251203@users.noreply.github.com>
2024-09-01 19:59:04 +0530

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -2,18 +2,24 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-
-contract MyOwnERC20Token is ERC20 {
-    constructor() ERC20("Dinar", "DNR")  {
+contract MyOwnERC20Token is ERC20 , Ownable{
+    constructor() ERC20("Dinar", "DNR") Ownable(msg.sender) {
         _mint(msg.sender, 1000 * 10 ** decimals()); 
     }
 
-    function mint(address recipient, uint256 amount) external  {
+    function mint(address recipient, uint256 amount) external onlyOwner {
         _mint(recipient, amount);
     }
 
     function burn(uint256 amount) external {
         _burn(msg.sender, amount);
+    }
+
+    function customTransfer(address from, address to, uint256 amount) public returns (bool) {
+        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
+        _transfer(from, to, amount);
+        return true;
     }
 }
